f3b23fac0bf682dd0a76116c68c24469900a67a6
Sean Anderson <ander28@pdx.edu>
2018-06-03 13:27:21 -0700

    Added midi output capability. Depends on midi server running on 128 0.

--- midi-tools/midi-tools.py
+++ midi-tools/midi-tools.py
@@ -1,6 +1,10 @@
 from midi import *
 from sys import argv
 from copy import deepcopy
+import midi.sequencer as sequencer
+import time
+import socket
+import os
 
 ##################################### Usage #########################################
 # fileToNotes reads a midi file and returns a list of notes in order without        #
@@ -44,7 +48,36 @@ def singleNote(notes):
   else: return notes[len(notes)-1]
 
 def fileToTickNotes(f):
-  return list(map(singleNote, fileToTicks(f))) 
+  return list(map(singleNote, fileToTicks(f)))
+
+def stageToSeq(tickNotes):
+  parent, child = socket.socket
+  client = 128
+  port = 0
+  seq = sequencer.SequencerWrite()
+  seq.subscribe_port(client, port)
+  seq.start_sequencer()
+  for tick in range(len(tickNotes)):
+    # create an event
+    buf = None
+    if (tick-1 < 0) or tickNotes[tick] != tickNotes[tick-1] and tickNotes[tick] != 0:
+      event = NoteOnEvent()
+      event.tick = 3*tick
+      event.set_pitch(tickNotes[tick])
+      event.set_velocity(40)
+      buf = seq.event_write(event, False, False, True)
+    if (tick+1 >= len(tickNotes)) or tickNotes[tick] != tickNotes[tick+1] and tickNotes[tick] != 0:
+      event = NoteOffEvent()
+      event.tick = 3*tick+1
+      event.set_pitch(tickNotes[tick])
+      event.set_velocity(40)
+      buf = seq.event_write(event, False, False, True)
+    if buf == None:
+      continue
+    if buf < 1000:
+      time.sleep(.1)
+  time.sleep(30)
 
 if len(argv) > 1:
-  print(list(map(singleNote, fileToTicks(argv[1]))))
+#  print(list(map(singleNote, fileToTicks(argv[1]))))
+  stageToSeq(list(map(singleNote, fileToTicks(argv[1]))))
